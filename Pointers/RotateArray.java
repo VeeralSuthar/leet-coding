b320bc56fa3888af3029974b478a9c67a944bb26
VeeralSuthar <vsuthar.vhs@gmail.com>
2022-07-14 20:41:40 -0500

    Algorithm I: Day 2 - 2 Pointers

---
+++ Pointers/RotateArray.java
@@ -0,0 +1,32 @@
+package Pointers;
+
+/*
+ * Title: Rotate Array
+ * 
+ * Problem: Given an array, rotate the array to the right by k steps, where k is non-negative.
+ * 
+ * Author: Veeral Suthar
+ *
+ * Date: 07/14/2022
+ * 
+ * Solution: Straight up right array rotate.
+ * Time Complexity: O(n*k)
+ */
+
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }   
+    public void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+        }
+    }
+}
