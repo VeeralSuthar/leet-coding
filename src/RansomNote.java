ef3ed8d9a28ff67b755df393ffb9705d8c11b5e4
VeeralSuthar <vsuthar.vhs@gmail.com>
2022-08-25 06:48:08 -0500

    for-indexed to for-each

--- src/RansomNote.java
+++ src/RansomNote.java
@@ -2,12 +2,12 @@ public class RansomNote {
     public boolean canConstruct(String ransomNote, String magazine) {
         int[] count = new int[26];
         
-        for (int i = 0; i < magazine.length(); i++) {
-            count[magazine.charAt(i)-'a']++;
+        for (char c : magazine.toCharArray()) {
+            count[c-'a']++;
         }
-        for (int i = 0; i < ransomNote.length(); i++) {
-            if (count[ransomNote.charAt(i)-'a'] <= 0) return false;
-            else count[ransomNote.charAt(i)-'a']--;
+        for (char c : ransomNote.toCharArray()) {
+            if (count[c-'a'] <= 0) return false;
+            else count[c-'a']--;
         }
         return true;
     }
